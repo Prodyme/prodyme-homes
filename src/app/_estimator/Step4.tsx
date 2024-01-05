3772ea1d793a7ad40fc286cb61a44e29fb9d36f5
Aman Singh Samant <56504833+Aman-Singh-Samant@users.noreply.github.com>
2024-01-05 23:43:06 +0530

    created estimator

---
+++ src/app/_estimator/Step4.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { step4Type, stepType } from "./_myTypes";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDFFile } from "./_components/PDFFile";
+
+export const Step4: React.FC<step4Type> = ({
+  step,
+  setStep,
+  estimateValue,
+  groupedItems,
+  setGroupedItems,
+  quality,
+  rateSum,
+  reset
+}) => {
+  const handleProceed = () => {
+    reset();
+  };
+
+  return (
+    <>
+      <div
+        className={`w-[899px] max-xl:w-[90vw] h-auto gap-[30px] pt-3  pb-6 bg-white rounded-bl-lg rounded-br-lg border border-orange-400 flex-col justify-center items-center inline-flex  ${
+          step !== 4 ? "hidden" : ""
+        }`}
+      >
+        <span className="text-orange-400 m-8 text-6xl font-bold font-['Anek Latin'] text-center">
+          Thank You
+        </span>
+        <div className="justify-center items-center gap-5 inline-flex w-[80vw] flex-wrap">
+          <PDFDownloadLink
+            document={
+              <PDFFile
+                step={step}
+                setStep={setStep}
+                estimateValue={estimateValue}
+                groupedItems={groupedItems}
+                setGroupedItems={setGroupedItems}
+                quality={quality}
+                rateSum={rateSum}
+              />
+            }
+            fileName="Generated Estimate"
+            className="w-[200px] px-10 py-2.5 bg-white rounded-[20px] border border-orange-400 justify-center items-center gap-2.5 flex transition-transform transform hover:scale-110"
+          >
+            <div className="text-orange-400 text-base font-normal font-['Anek Latin']">
+              Download
+            </div>
+          </PDFDownloadLink>
+          <button
+            onClick={handleProceed}
+            className="w-[300px] px-10 py-2.5 bg-orange-400 rounded-[20px] justify-center items-center gap-2.5 flex transition-transform transform hover:scale-110"
+          >
+            <div className="text-white text-base font-normal font-['Anek Latin']">
+              Generate New Budget
+            </div>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 import Image from 'next/image'
+import React from "react";
+import Estimator from './_estimator/Estimator';
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-        <header className="pb-24">
-            <h4>Prodyme</h4>
-        </header>
-        <figure>
-            <Image
-                src="/images/logo.png"
-                alt="prodyme homes"
-                width={180}
-                height={37}
-                priority
-            />
-        </figure>
+        <Estimator />
     </main>
   )
 }
