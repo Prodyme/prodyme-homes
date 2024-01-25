eba91b36c22840584fe9ff85acec9a50577a0313
Aman Singh Samant <56504833+Aman-Singh-Samant@users.noreply.github.com>
2024-01-26 02:34:00 +0530

    changed left bar to sticky and width of step 4 to dynamic

--- src/app/components/_estimator/Estimator.tsx
+++ src/app/components/_estimator/Estimator.tsx
@@ -51,7 +51,7 @@ const Estimator = () => {
 
   useEffect(() => {
     console.log(groupedItems, "hello");
-    if(flag === true){
+    if (flag === true) {
       generateEstimate();
     }
   }, [groupedItems]);
@@ -63,7 +63,7 @@ const Estimator = () => {
     });
     // console.log(sourceData)
     setGroupedItems(sourceData);
-    
+
     // console.log(groupedItems);
   }
 
@@ -88,20 +88,20 @@ const Estimator = () => {
     if (Number.isNaN(basementArea)) {
       totalArea = buildUpArea;
     } else {
-      totalArea = (basementArea*1.5) + buildUpArea;
+      totalArea = basementArea * 1.5 + buildUpArea;
     }
     groupedItems.map((items) => {
       items.items.map((item) => {
-        if(items.Category === item.Category){
-          if(typeof item.Rate === 'number' && item.Rate !== 0){
+        if (items.Category === item.Category) {
+          if (typeof item.Rate === "number" && item.Rate !== 0) {
             totalRate += item.Rate;
-          } 
+          }
         }
-      })
-    })
-    setCostPerSqft((totalArea*totalRate)/buildUpArea)
+      });
+    });
+    setCostPerSqft((totalArea * totalRate) / buildUpArea);
     setRateSum(totalRate);
-    setEstimateValue(totalArea*totalRate);
+    setEstimateValue(totalArea * totalRate);
     setFlag(true);
   };
 
@@ -113,11 +113,11 @@ const Estimator = () => {
     setRateSum(0);
     setEstimateValue(0);
     setResetFlag(false);
-  }
+  };
 
   useEffect(() => {
     setResetFlag(true);
-  },[resetFlag])
+  }, [resetFlag]);
 
   return (
     <>
@@ -138,10 +138,18 @@ const Estimator = () => {
 
           <div className="flex max-xl:flex-col h-auto ">
             {" "}
-            <SideBar step={step} setStep={setStep} buildUpArea={buildUpArea} quality={quality}/>
+            <div className="flex-grow">
+              {" "}
+              <SideBar
+                step={step}
+                setStep={setStep}
+                buildUpArea={buildUpArea}
+                quality={quality}
+              />
+            </div>
             <div className="flex flex-col">
               <div
-                className={`border-4 h-0 border-orange-400 transition-all ease-out duration-1000 m-0 sticky top-0 ${
+                className={`border-4 h-0 border-orange-400 transition-all ease-out duration-1000 m-0 ${
                   step === 1
                     ? "w-[5vw]"
                     : step === 2
--- src/app/components/_estimator/SideBar.tsx
+++ src/app/components/_estimator/SideBar.tsx
@@ -17,7 +17,7 @@ export const SideBar: React.FC<stepType> = ({ step, setStep, buildUpArea, qualit
     }
   }, [quality]);
   return (
-    <div className="mt-2 flex min-[1280px]:flex-col text-center ">
+    <div className="mt-2 flex min-[1280px]:flex-col text-center sticky top-[20px]">
       <div
         className={`${
           step === 1
@@ -43,7 +43,7 @@ export const SideBar: React.FC<stepType> = ({ step, setStep, buildUpArea, qualit
             : "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-zinc-300  justify-end items-start gap-2.5 inline-flex text-black text-xl max-sm:text-base font-normal font-['Anek Latin']"
         }
       >
-        <div className="">Select Premium </div>
+        <div className="">{qValue} </div>
         <div className={step <= 2 ? " hidden" : "w-[26px] h-[26px] relative max-sm:hidden"}>
           <img
             className="w-[26px] h-[26px] left-0 top-0 object-cover absolute rounded-full border-2 border-black"
@@ -55,14 +55,14 @@ export const SideBar: React.FC<stepType> = ({ step, setStep, buildUpArea, qualit
       <div
         className={
           step <= 2
-            ? "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6  min-[1280px]:rounded-bl-lg max-xl:rounded-r-lg justify-end items-start gap-2.5 inline-flex text-orange-400 text-xl max-sm:text-base font-normal font-['Anek Latin']"
+            ? "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6  min-[1280px]:rounded-bl-lg max-xl:rounded-r-lg justify-end items-start gap-2.5 inline-flex text-orange-400 text-xl max-sm:text-base font-normal font-['Anek Latin'] max-sm:pl-24"
             : step === 3
-            ? "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-orange-400  min-[1280px]:rounded-bl-lg max-xl:rounded-r-lg justify-end items-start gap-2.5 inline-flex text-white text-xl max-sm:text-base font-bold font-['Anek Latin']"
-            : "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-zinc-300  justify-end items-start gap-2.5 inline-flex text-black text-xl max-sm:text-base font-normal font-['Anek Latin']"
+            ? "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-orange-400  min-[1280px]:rounded-bl-lg max-xl:rounded-r-lg justify-end items-start gap-2.5 inline-flex text-white text-xl max-sm:text-base font-bold font-['Anek Latin'] max-sm:pl-24 max-sm:mx-0 "
+            : "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-zinc-300  justify-end items-start gap-2.5 inline-flex text-black text-xl max-sm:text-base font-normal font-['Anek Latin'] max-sm:pl-24 max-sm:mx-0"
         }
       >
         <div className="">Customize Estimator</div>
-        <div className={step <= 3 ? " hidden" : "w-[26px] h-[26px] relative max-sm:hidden"}>
+        <div className={step <= 3 ? " hidden" : "w-[26px] h-[26px] relative max-sm:hidden "}>
           <img
             className="w-[26px] h-[26px] object-cover left-0 top-0 absolute rounded-full border-2 border-black"
             src="images/ok.svg"
