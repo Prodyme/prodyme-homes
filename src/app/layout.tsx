dd25e11eb496d2e24e13f1e11895ad404f9c7d3c
Aman Singh Samant <56504833+Aman-Singh-Samant@users.noreply.github.com>
2024-02-01 19:38:44 +0530

    added roboto and inter font as primary and secondary fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const roboto = Roboto({
+  weight: ["400", "700", "100", "300", "500", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "Prodyme Homes",
@@ -19,16 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" as="stylesheet" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" as="stylesheet" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-          as="stylesheet"
-        />
-      </head>
-      <body className={inter.className}>
+      <body className={`${inter.variable} ${roboto.variable}`}>
         <Navbar />
         {children}
         <Footer />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        sans: [ "Roboto", "sans-serif"],
+        sans: ['var(--font-roboto)', 'var(--font-inter)'],
       },
     },
   },
