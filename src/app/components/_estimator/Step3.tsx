02c595fd0a57c8248471ea55c4878e157962ad03
Aman Singh Samant <56504833+Aman-Singh-Samant@users.noreply.github.com>
2024-02-09 12:50:33 +0530

    resolved bug

--- src/app/components/_estimator/Step3.tsx
+++ src/app/components/_estimator/Step3.tsx
@@ -93,8 +93,10 @@ const Step3: React.FC<step3Type> = ({
               "Content-Type": "application/json",
               // Add any additional headers if needed
             },
-            body: JSON.stringify({ "email": popUpFormData.email, "phone": popUpFormData.phoneNo }),
-
+            body: JSON.stringify({
+              email: popUpFormData.email,
+              phone: popUpFormData.phoneNo,
+            }),
           }
         );
 
@@ -165,42 +167,40 @@ const Step3: React.FC<step3Type> = ({
             <span className="text-red-500">{errors.email}</span>
           </div>
 
-            <div className="justify-center items-center flex flex-col m-3">
-              <label className="my-1" htmlFor="phoneNo">
-                Phone Number:{" "}
-              </label>
-              <input
-                placeholder="Phone Number"
-                className="border rounded-lg p-1 text-black"
-                type="tel"
-                id="phoneNo"
-                name="phoneNo"
-                defaultValue={popUpFormData.phoneNo}
-                onChange={handleChange}
-              />{" "}
-              <span className="text-red-500">{errors.phoneNo}</span>
-            </div>
+          <div className="justify-center items-center flex flex-col m-3">
+            <label className="my-1" htmlFor="phoneNo">
+              Phone Number:{" "}
+            </label>
+            <input
+              placeholder="Phone Number"
+              className="border rounded-lg p-1 text-black"
+              type="tel"
+              id="phoneNo"
+              name="phoneNo"
+              defaultValue={popUpFormData.phoneNo}
+              onChange={handleChange}
+            />{" "}
+            <span className="text-red-500">{errors.phoneNo}</span>
+          </div>
 
-            <button
-              type="submit"
-              className="w-[135px] px-10 py-2.5 bg-orange-400 rounded-[20px] justify-center items-center gap-2.5 flex transition-transform transform hover:scale-110"
-            >
-              <div className="text-white text-base font-medium font-['Anek Latin']">
-                Submit
-              </div>
-            </button>
-          </form>
           <button
-            className="text-red-500 bg-white font-bold text-lg mt-[-500px] ml-[-20px] rounded-full border border-red-400 px-2 "
-            onClick={() => setPopUpFormFlag(false)}
+            type="submit"
+            className="w-[135px] px-10 py-2.5 bg-orange-400 rounded-[20px] justify-center items-center gap-2.5 flex transition-transform transform hover:scale-110"
           >
-            X
+            <div className="text-white text-base font-medium font-['Anek Latin']">
+              Submit
+            </div>
           </button>
-        </div>
+        </form>
+        <button
+          className="text-red-500 bg-white font-bold text-lg mt-[-500px] ml-[-20px] rounded-full border border-red-400 px-2 "
+          onClick={() => setPopUpFormFlag(false)}
+        >
+          X
+        </button>
       </div>
     );
   };
-
   const handleProceed = () => {
     setPopUpFormFlag(true);
   };
@@ -314,140 +314,143 @@ const Step3: React.FC<step3Type> = ({
                       {d.itemName}
                     </div>
 
-                      {/* <div className="text-black md:text-sm text-xs font-normal font-['Anek Latin']">
+                    {/* <div className="text-black md:text-sm text-xs font-normal font-['Anek Latin']">
                       Rathi, Kamdhenu or equivalent
                     </div> */}
-                    </div>
-                    <div className="flex">
-                      <button
-                        onClick={() => handleOptions(d)}
-                        className="transition-transform transform hover:scale-110 w-[30px] object-contain"
-                      >
-                        <Image
-                          src="images/pen.svg"
-                          alt="Detailed Description"
-                          width="0"
-                          height="0"
-                          sizes="100vw"
-                          className="w-[20px] h-auto"
-                        />
-                      </button>
-                      <button
-                        onClick={() => showDesc(d)}
-                        className="transition-transform transform hover:scale-110 w-[30px] object-contain"
-                      >
-                        <Image
-                          src="images/info.svg"
-                          alt="Detailed Description"
-                          width="0"
-                          height="0"
-                          sizes="100vw"
-                          className="w-[20px] h-auto"
-                        />
-                      </button>
-                      {info.name === d.itemName && (
-                        <div
-                          className={`absolute z-50 font-normal bg-orange-50 mt-8 justify-center m-auto p-1 w-[400px]   max-xl:ml-[-25vw] max-sm:mx-2 max-sm:w-[90vw] max-sm:left-[2vw]  rounded-xl text-wrap ${(i + 1) % 3 === 0
-                              ? "ml-[-350px] max-xl:ml-[-25vw]"
-                              : ""
-                            }`}
-                        >
-                          {info.desc}
-                        </div>
-                      )}
-                    </div>
                   </div>
-                  <span className="text-black md:text-sm text-xs mr-2 w-[200px] font-font-normal font-['Anek Latin']">
-                    {d.items.map((i, index) => {
-                      if (d.Category === i.Category) {
-                        return (
-                          <div key={index}>
-                            {i.Description.slice(0, 60)}{" "}
-                            {i.Description.length > 60 ? " ..." : ""}
-                          </div>
-                        );
-                      }
-                    })}
-                  </span>
-                </div>
-                <button
-                  onClick={() => handleOptions(d)}
-                  className="w-[240px] max-sm:w-[200px] h-[50px] p-2.5 bg-neutral-100 rounded-lg border border-orange-400 justify-start items-center gap-2.5 inline-flex transition-transform transform hover:scale-110"
-                >
-                  <div className="grow shrink basis-0 ">
-                    {d.items.map((i, index) => {
-                      if (d.Category === i.Category) {
-                        return (
-                          <span key={index}>
-                            <span className="text-black md:text-sm text-xs font-bold font-['Anek Latin']">
-                              INR
-                            </span>{" "}
-                            {i.Rate}
-                            <span className="text-black md:text-sm text-xs font-font-normal font-['Anek Latin']">
-                              {" "}
-                              {i.Unit}
-                            </span>
-                          </span>
-                        );
-                      }
-                    })}
-                  </div>
-                </button>
-
-                <div
-                  className={`${d.Selected === true
-                      ? `absolute z-50 font-normal bg-gray-50 mt-40 justify-center m-auto p-1 w-[400px] max-sm:mx-2 max-sm:w-[85vw] max-sm:left-[2vw]  rounded-xl text-wrap ${(i + 1) % 3 === 0 ? "" : ""
-                      }`
-                      : "hidden"
-                    }`}
-                >
-                  {" "}
-                  <span className="font-bold text-orange-500 md:text-2xl text-base">
-                    Choose product
-                  </span>
-                  {d.items.map((item, i) => {
-                    return (
+                  <div className="flex">
+                    <button
+                      onClick={() => handleOptions(d)}
+                      className="transition-transform transform hover:scale-110 w-[30px] object-contain"
+                    >
+                      <Image
+                        src="images/pen.svg"
+                        alt="Detailed Description"
+                        width="0"
+                        height="0"
+                        sizes="100vw"
+                        className="w-[20px] h-auto"
+                      />
+                    </button>
+                    <button
+                      onClick={() => showDesc(d)}
+                      className="transition-transform transform hover:scale-110 w-[30px] object-contain"
+                    >
+                      <Image
+                        src="images/info.svg"
+                        alt="Detailed Description"
+                        width="0"
+                        height="0"
+                        sizes="100vw"
+                        className="w-[20px] h-auto"
+                      />
+                    </button>
+                    {info.name === d.itemName && (
                       <div
-                        className="my-5  flex flex-col m-auto justify-center"
-                        key={i}
+                        className={`absolute z-50 font-normal bg-orange-50 mt-8 justify-center m-auto p-1 w-[400px]   max-xl:ml-[-25vw] max-sm:mx-2 max-sm:w-[90vw] max-sm:left-[2vw]  rounded-xl text-wrap ${
+                          (i + 1) % 3 === 0
+                            ? "ml-[-350px] max-xl:ml-[-25vw]"
+                            : ""
+                        }`}
                       >
-                        <label
-                          className="bg-gradient-to-r bg-white
+                        {info.desc}
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <span className="text-black md:text-sm text-xs mr-2 w-[200px] font-font-normal font-['Anek Latin']">
+                  {d.items.map((i, index) => {
+                    if (d.Category === i.Category) {
+                      return (
+                        <div key={index}>
+                          {i.Description.slice(0, 60)}{" "}
+                          {i.Description.length > 60 ? " ..." : ""}
+                        </div>
+                      );
+                    }
+                  })}
+                </span>
+              </div>
+              <button
+                onClick={() => handleOptions(d)}
+                className="w-[240px] max-sm:w-[200px] h-[50px] p-2.5 bg-neutral-100 rounded-lg border border-orange-400 justify-start items-center gap-2.5 inline-flex transition-transform transform hover:scale-110"
+              >
+                <div className="grow shrink basis-0 ">
+                  {d.items.map((i, index) => {
+                    if (d.Category === i.Category) {
+                      return (
+                        <span key={index}>
+                          <span className="text-black md:text-sm text-xs font-bold font-['Anek Latin']">
+                            INR
+                          </span>{" "}
+                          {i.Rate}
+                          <span className="text-black md:text-sm text-xs font-font-normal font-['Anek Latin']">
+                            {" "}
+                            {i.Unit}
+                          </span>
+                        </span>
+                      );
+                    }
+                  })}
+                </div>
+              </button>
+
+              <div
+                className={`${
+                  d.Selected === true
+                    ? `absolute z-50 font-normal bg-gray-50 mt-40 justify-center m-auto p-1 w-[400px] max-sm:mx-2 max-sm:w-[85vw] max-sm:left-[2vw]  rounded-xl text-wrap ${
+                        (i + 1) % 3 === 0 ? "" : ""
+                      }`
+                    : "hidden"
+                }`}
+              >
+                {" "}
+                <span className="font-bold text-orange-500 md:text-2xl text-base">
+                  Choose product
+                </span>
+                {d.items.map((item, i) => {
+                  return (
+                    <div
+                      className="my-5  flex flex-col m-auto justify-center"
+                      key={i}
+                    >
+                      <label
+                        className="bg-gradient-to-r bg-white
         hover:from-amber-50 hover:via-amber-100 
         hover:to-amber-50 text-[1rem] font-normal 
         justify-center m-auto p-1 w-[300px]  max-sm:m-2 max-sm:w-[80vw]
         hover:shadow-[0_10px_20px_rgba(255,193,150,0.9)]
         shadow-[0_10px_20px_rgba(231,229,228,0.9)]
         rounded-xl text-wrap"
-                          htmlFor={i.toString()}
-                        >
-                          <span className="font-bold">Description: </span>
-                          <span>{item.Description}</span>
-                          <br />
-                          <span className="font-bold">Rate: </span>
-                          <span>INR {item.Rate}</span>
-                        </label>
-                        <input
-                          type="radio"
-                          name={item.Item}
-                          value={item.Rate}
-                          id={i.toString()}
-                          onClick={() => onChangeHandle(item)}
-                          className="hidden"
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-
-                {(i + 1) % 3 !== 0 && i !== groupedItems.length - 1 && (
-                  <div className=" max-lg:hidden w-[0px] h-full absolute right-0 top-0 origin-top-left border border-stone-300"></div>
-                )}
+                        htmlFor={i.toString()}
+                      >
+                        <span className="font-bold">Description: </span>
+                        <span>{item.Description}</span>
+                        <br />
+                        <span className="font-bold">Rate: </span>
+                        <span>INR {item.Rate}</span>
+                      </label>
+                      <input
+                        type="radio"
+                        name={item.Item}
+                        value={item.Rate}
+                        id={i.toString()}
+                        onClick={() => onChangeHandle(item)}
+                        className="hidden"
+                      />
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-        <div className="w-[800px] h-[0px] max-xl:w-[80vw] mt-4 border border-stone-300"></div>
+
+              {(i + 1) % 3 !== 0 && i !== groupedItems.length - 1 && (
+                <div className=" max-lg:hidden w-[0px] h-full absolute right-0 top-0 origin-top-left border border-stone-300"></div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="w-[800px] h-[0px] max-xl:w-[80vw] mt-4 border border-stone-300"></div>
 
       <button
         onClick={handleReset}
