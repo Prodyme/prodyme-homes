bf6454fc6619d69e7e255bb772b719983197b21d
Nakul Talwar <117070626+NakulTalwar10@users.noreply.github.com>
2024-02-09 18:41:21 +0530

    Update image domains and remove unoptimized flag

--- next.config.js
+++ next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com','static.wixstatic.com' ,'example.com', 'anotherdomain.com','media.istockphoto.com'],
         // domains: ['media.istockphoto.com', 'example.com', 'anotherdomain.com'],
-<<<<<<< HEAD
         unoptimized: true
-=======
-        unoptimized: true,
->>>>>>> 36c433b3907c17e7a88b57a54f3385d781cf6173
       },
 }
 
