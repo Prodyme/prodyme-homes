b3751e8c36d6967edf86434d1903a05c54760514
Nakul Talwar <117070626+NakulTalwar10@users.noreply.github.com>
2024-02-19 12:06:44 +0530

    Update package.json, package-lock.json, and SideBar.tsx

--- src/app/components/_estimator/SideBar.tsx
+++ src/app/components/_estimator/SideBar.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { stepType } from './_myTypes';
 import Image from 'next/image';
-import { Element } from 'react-scroll';
 
-export const SideBar: React.FC<stepType> = ({ step, setStep, buildUpArea, quality }) => {
+export const SideBar: React.FC<stepType> = ({ step, setStep, buildUpArea, quality })=> {
   const [qValue, setQValue] = useState("");
   useEffect(() => {
     if (quality === 1) {
@@ -85,27 +84,7 @@ export const SideBar: React.FC<stepType> = ({ step, setStep, buildUpArea, qualit
             alt="ok"
           />
         </div>
-        <div
-          className={
-            step <= 2
-              ? "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6  min-[1280px]:rounded-bl-lg max-xl:rounded-r-lg justify-end max-sm:justify-end max-xl:justify-center items-start gap-2.5 inline-flex text-orange-400 md:text-xl text-base max-sm:text-base font-normal font-['Anek Latin'] max-sm:pl-24"
-              : step === 3
-                ? "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-orange-400  min-[1280px]:rounded-bl-lg max-xl:rounded-r-lg justify-end max-sm:justify-end max-xl:justify-center items-start gap-2.5 inline-flex text-white md:text-xl text-base max-sm:text-base font-bold font-['Anek Latin'] max-sm:pl-24 max-sm:mx-0 "
-                : "w-[300px] max-xl:w-[30vw] h-[80px] px-5 py-6 bg-zinc-300  justify-end max-sm:justify-end max-xl:justify-center items-start gap-2.5 inline-flex text-black md:text-xl text-base max-sm:text-base font-normal font-['Anek Latin'] max-sm:pl-24 max-sm:mx-0"
-          }
-        >
-          <div className="">Customize Estimator</div>
-          <div className={step <= 3 ? " hidden" : "w-[26px] h-[26px] relative max-sm:hidden "}>
-            <img
-              className="w-[26px] h-[26px] object-cover left-0 top-0 absolute rounded-full border-2 border-black"
-              src="images/ok.svg"
-              alt="ok"
-
-            />
-          </div>
-        </div>
-
       </div>
-   </div>
+    </div>
   );
 }
